4cbf1d91a22396e786589d7ea61c9a5c5314b99f
John H Moore <john@jhmwebdesign.com>
2013-12-11 19:22:02 -0500

    Registered Blog and Category models for admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from blog.models import Blog, Category
+
+class BlogAdmin(admin.ModelAdmin):
+	exclude = ['posted']
+	prepopulated_fields = {'slug': ('title',)}
+
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('title',)}
+
 
 # Register your models here.
+admin.site.register(Blog)
+admin.site.register(Category)
